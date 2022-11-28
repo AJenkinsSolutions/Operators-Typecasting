aacfb31dc22bf91381dddb5ada29e40c6aaf6288
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2022-11-28 07:27:56 -0600

    Add loops Practise assignment 303.3.1

---
+++ src/com/perscholas/loopsassignment/LoopsPracticeAssignment.java
@@ -0,0 +1,63 @@
+package com.perscholas.loopsassignment;
+
+import java.awt.desktop.AboutHandler;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import javax.swing.event.AncestorEvent;
+
+public class LoopsPracticeAssignment {
+
+	public static void getTimesTable() {
+		int start = 1; 
+		
+		
+		for(int i=1; i <=12; i ++) {
+			System.out.println(" ");
+			for(int j =1; j<=12; j ++) {
+				System.out.print(" "+ j * i+ " ");
+			}
+		}
+		System.out.println(" \n");
+
+	}
+	
+	public static void getGreatCommonDivisor() {
+		Scanner inputScanner = new Scanner(System.in);
+		int[] userInput = new int[2];
+		
+		System.out.println("Enter a integer");
+		
+		
+		for(int i=0; i<userInput.length; i ++) {
+			userInput[i] = inputScanner.nextInt();
+		}
+		System.out.println("You choose" + Arrays.toString(userInput));
+		
+		
+		int n1 = userInput[0];
+		int n2 = userInput[1];
+		
+		int gcd=1;
+		for(int i=2; i<Math.max(userInput[0], userInput[1]); i++) {
+			if(n1%i == 0 && n2 %i ==0){
+				gcd = i;
+			
+			}
+
+		}
+		System.out.println("Greatest Common Divisor " + gcd);
+		
+		
+		
+		
+	}
+	public static void main(String[] args) {
+		getTimesTable();
+		
+		getGreatCommonDivisor();
+		
+
+	}
+
+}
