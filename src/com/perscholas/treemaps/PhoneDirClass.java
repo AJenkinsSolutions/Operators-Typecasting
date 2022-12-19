c05c9fd199074f0831843951ba5d13e02e45342c
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2022-12-19 09:11:20 -0600

    First commit after new ssh key

---
+++ src/com/perscholas/treemaps/PhoneDirClass.java
@@ -0,0 +1,26 @@
+package com.perscholas.treemaps;
+
+import java.util.TreeMap;
+
+public class PhoneDirClass {
+	
+	public PhoneDirClass() {
+		//Initialize empty tree map 
+		this.data = new TreeMap<>();
+	}
+
+
+
+
+	private TreeMap<String, String> data;
+	
+	
+	
+
+	public TreeMap<String, String> getData() {
+		return data;
+	} 
+	
+	
+	
+}
---
+++ src/com/perscholas/treemaps/PhoneDirectoryDriver.java
@@ -0,0 +1,16 @@
+package com.perscholas.treemaps;
+
+public class PhoneDirectoryDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		PhoneDirClass dir = new PhoneDirClass();
+		
+		System.out.println(dir.getData());
+		
+	}
+
+}
